d893912bc0cc3a52c7399efe2a02e804d0802ca7
rodrigo <mcd77.1990@gmail.com>
2019-10-18 17:36:30 -0300

    seed  + migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,120 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+   
+    	DB::table('marca')
+     	->insert([
+            'id' => 1,
+            'descripcion' => 'Vans'
+        ]);
+
+        DB::table('marca')
+     	->insert([
+            'id' => 2,
+            'descripcion' => 'Nike'
+        ]);
+
+        DB::table('marca')
+     	->insert([
+            'id' => 3,
+            'descripcion' => 'Running'
+        ]);
+     	DB::table('marca')
+        ->insert([
+            'id' => 4,
+            'descripcion' => 'Skate'
+        ]);
+
+
+        DB::table('categoria')
+     	->insert([
+            'id' => 1,
+            'descripcion' => 'Calzado'
+        ]);
+
+        DB::table('categoria')
+     	->insert([
+            'id' => 2,
+            'descripcion' => 'Calzado'
+        ]);
+
+        DB::table('categoria')
+     	->insert([
+            'id' => 3,
+            'descripcion' => 'Calzado'
+        ]);
+     	DB::table('categoria')
+        ->insert([
+            'id' => 4,
+            'descripcion' => 'Calzado'
+        ]);
+
+
+
+     	DB::table('producto')
+     	->insert([
+            'modelo' => 'Old School',
+            'marca_id' => 'Vans',
+            'precio' => 245.50,
+            'marca_id' => 1,
+            'categoria_id' => 1
+        ]);
+        DB::table('producto')
+     	->insert([
+            'modelo' => 'Outburst',
+            'marca_id' => 'Nike',
+            'precio' => 560.55,
+            'marca_id' => 2,
+            'categoria_id' => 2
+        ]);
+        DB::table('producto')
+     	->insert([
+            'modelo' => 'Adidas',
+            'marca_id' => 'Running',
+            'precio' => 245.50,
+            'marca_id' => 3,
+            'categoria_id' => 3
+        ]);
+     	DB::table('producto')
+        ->insert([
+            'modelo' => 'DC',
+            'marca_id' => 'Skate',
+            'precio' => 1500.99,
+            'marca_id' => 4,
+            'categoria_id' => 4
+        ]);
+
+
+        DB::table('imagen_producto')
+     	->insert([
+            'nombre_archivo' => 'oldschool.webp',
+            'producto_id' => 1,
+            'tipo_id' => 0
+        ]);
+        DB::table('imagen_producto')
+        ->insert([
+            'nombre_archivo' => 'outburst.webp',
+            'producto_id' => 2,
+            'tipo_id' => 0
+        ]);
+        DB::table('imagen_producto')
+        ->insert([
+            'nombre_archivo' => 'running.webp',
+            'producto_id' => 3,
+            'tipo_id' => 0
+        ]);
+        DB::table('imagen_producto')
+        ->insert([
+            'nombre_archivo' => 'running.webp',
+            'producto_id' => 3,
+            'tipo_id' => 0
+        ]);
+        DB::table('imagen_producto')
+        ->insert([
+            'nombre_archivo' => 'skate.webp',
+            'producto_id' => 4,
+            'tipo_id' => 0
+        ]);
+    
     }
 }
